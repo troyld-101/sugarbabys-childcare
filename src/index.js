a4bbbe69e48b4eb71a0d945b33759c84810068a8
Troy Davis <troyld.101@gmail.com>
2026-02-25 14:54:29 -0600

    Update Package.json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import App from './App';
-//import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Import HashRouter instead
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</React.StrictMode>,
 );
-
